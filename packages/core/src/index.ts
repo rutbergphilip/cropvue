81f187178b574f01eb68c9eb491d4c38b44426cf
Philip Rutberg <54883264+rutbergphilip@users.noreply.github.com>
2026-02-13 16:17:05 +0100

    feat(core): finalize barrel exports and build configuration

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,35 @@
+// Composables
 export { useCropper } from './composables/useCropper'
-export { useDropzone } from './composables/useDropzone'
-export { useImageQueue } from './composables/useImageQueue'
-export { useUploader } from './composables/useUploader'
-export { useCompressor } from './composables/useCompressor'
+export { useDropzone, validateFile } from './composables/useDropzone'
+export { useImageQueue, createQueue } from './composables/useImageQueue'
+export { useUploader, createUploadHandler } from './composables/useUploader'
+export { useCompressor, chooseOutputFormat, compressBlob } from './composables/useCompressor'
+
+// Engine (for advanced usage)
+export {
+  createTransformState,
+  createCropState,
+  applyRotation,
+  applyFlip,
+  applyZoom,
+  applyPan,
+  clampTransform,
+  resetTransform,
+  snapRotation,
+} from './engine/transform'
+export { renderCrop, exportCrop } from './engine/canvas-renderer'
+export { constrainCropSize, constrainAspectRatio, constrainCropPosition } from './engine/constraints'
+export {
+  getRectangleClipPath,
+  getCircleClipPath,
+  getFreeformClipPath,
+  isPointInsideStencil,
+} from './engine/stencils'
+
+// Utils
+export { getMaxCanvasSize, downsampleDimensions } from './utils/canvas-limits'
+export { detectMimeType, supportsWebP, getMimeForFormat, hasTransparency } from './utils/format-detect'
+export { loadImageFromFile, loadImageFromUrl, needsDownsample, getSafeDimensions } from './utils/image-loader'
+
+// Types
 export type * from './types'
